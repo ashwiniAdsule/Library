896da9eeb789ca8187e8fa577bcd0932e43976ab
ashwiniAdsule <the.ashwini.adsule@gmail.com>
2022-09-18 17:49:43 +0530

    my books

---
+++ script.js
@@ -0,0 +1,53 @@
+function changeBodyBg(color) {
+    document.body.style.background = color;
+}
+
+function myFunction1() {
+    document.getElementById("changeImage").src = "https://images-na.ssl-images-amazon.com/images/I/91-EIJiYneL.jpg";
+    document.getElementById("name1").style.color = "red";
+}
+
+function myFunction2() {
+    document.getElementById("changeImage").src = "https://m.media-amazon.com/images/I/51AHZGhzZEL._SL500_.jpg";
+    document.getElementById("name2").style.color = "red";
+}
+
+function myFunction3() {
+    document.getElementById("changeImage").src = "https://images-na.ssl-images-amazon.com/images/I/51QTTApN-XL._SX324_BO1,204,203,200_.jpg";
+    document.getElementById("name3").style.color = "red";
+}
+
+function myFunction4() {
+    document.getElementById("changeImage").src = "https://i1.wp.com/www.samuelthomasdavies.com/wp-content/uploads/2017/11/The-Subtle-Art-of-Not-Giving-a-Fck-Summary.png?resize=152%2C225&ssl=1";
+    document.getElementById("name4").style.color = "red";
+}
+
+function myFunction5() {
+    document.getElementById("changeImage").src = "https://images-na.ssl-images-amazon.com/images/I/41r6F2LRf8L._SX323_BO1,204,203,200_.jpg";
+    document.getElementById("name5").style.color = "red";
+}
+
+function myFunction6() {
+    document.getElementById("changeImage").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQH8zw2iwSfwW3l3F1moklgy7STUGBXQvWHOQ&usqp=CAU";
+    document.getElementById("name6").style.color = "red";
+}
+
+function myFunction7() {
+    document.getElementById("changeImage").src = "https://images-na.ssl-images-amazon.com/images/I/41d-tPCjO+L._SX326_BO1,204,203,200_.jpg";
+    document.getElementById("name7").style.color = "red";
+}
+
+function myFunction8() {
+    document.getElementById("changeImage").src = "http://www.freepdfbook.com/wp-content/uploads/2020/07/d51738da77a36e688276df5329cb77d8.png";
+    document.getElementById("name8").style.color = "red";
+}
+
+function myFunction9() {
+    document.getElementById("changeImage").src = "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1387655636l/482579.jpg";
+    document.getElementById("name9").style.color = "red";
+}
+
+function myFunction10() {
+    document.getElementById("changeImage").src = "https://images-na.ssl-images-amazon.com/images/I/61CNHZBzqgL.jpg";
+    document.getElementById("name10").style.color = "red";
+}
